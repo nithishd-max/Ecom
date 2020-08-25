3ff0fccb8264d868e67ac8a60f171561fcb5618f
nithishdnr <nithishdnr3@gmail.com>
2020-08-25 14:32:13 +0530

    First commit

--- api/views.py
+++ api/views.py
@@ -3,4 +3,4 @@ from django.http import JsonResponse
 # Create your views here.
 
 def home(request):
-    return JsonResponse({'info':'Django React Course','name': "nitish"})
+    return JsonResponse({'info':'Django React Course','name': "nithish"})
